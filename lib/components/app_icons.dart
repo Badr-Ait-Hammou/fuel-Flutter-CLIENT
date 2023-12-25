d5405fa291cbebe56eace54daaa736f168201436
AnasJeg <anasjegoual.2000@gmail.com>
2023-12-25 15:09:22 +0100

    before-pull-dash

--- lib/components/app_icons.dart
+++ lib/components/app_icons.dart
@@ -1,8 +1,15 @@
 class AppIcons {
   static const String slpashIcon = 'assets/images/img_vector.svg';
+<<<<<<< HEAD
   static const String imgArrowdown = 'assets/images/img_arrowdown.svg';
   static const String imgLock = 'assets/images/img_lock.svg';
   static const String imgGoogle = 'assets/images/img_google.svg';
 
 
+=======
+  static const String imgHome = '';
+  static const String imgNavcard = '';
+  static const String imgNavactivity = '';
+  static const String imgNavprofile = '';
+>>>>>>> 083cbab (before-pull-dash)
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fuelflutter/pages/Login_Page.dart';
-import 'package:fuelflutter/pages/SignUp_Page.dart';
 import 'package:fuelflutter/pages/splash_page.dart';
 
 class Routes {
   static const String splashScreen = '/splash';
-  static const String loginScreen = '/login';
-  static const String signUpScreen = '/signup';
 
   static Map<String, WidgetBuilder> routes = {
-    splashScreen: (context) => SplashPage(),
-    loginScreen: (context) => LoginPage(),
-    signUpScreen: (context) => SignUpPage(),
+    splashScreen: (context) => SplashPage()
   };
 }
